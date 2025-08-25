773c0af70d6a83567608da437ab4e4a2353f543b
kstephens0331 <Kyle_stephens31@icloud.com>
2025-08-25 18:45:27 -0500

    update

--- scvpn-web/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}', // include js/jsx too
-  ],
-  theme: { extend: {} },
-  plugins: [require('tailwindcss-animate')],
-}
